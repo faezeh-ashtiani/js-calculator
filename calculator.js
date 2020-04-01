57de1e78e5e3c8628a98962e56c340f8cced590a
Faezeh Ashtiani <faa@Faezehs-MacBook-Pro.local>
2020-03-31 20:53:58 -0700

    cleaned up and works with prompt

--- calculator.js
+++ calculator.js
@@ -1,63 +1,79 @@
-// const exampleAdditionInput = {
-//   num1: 3,
-//   num2: 5,
-//   operation: 'add',
-// }
+const prompt = require('prompt');
 
-console.log("I am glad you chose my calculator, because it's awesome!");
-console.log("Which operator would you like to use?");
-console.log("1. add(+)");
-console.log("2. subtract(-)");
-console.log("3. multiply(*)");
-console.log("4. divide (/)");
-console.log('\(my program can read your mind and knows what you want to do!');
-console.log('I also know the numbers you have in mind!\)');
+let userInput = {
+  num1: "",
+  num2: "",
+  operation: "",
+};
 
-const operators = ["add", "+", "subtract", "-",
-"multiply", "*", "devide", "/"];
-
-// this is where we are reading users mind - feel free to change the values.
-const userInput = {
-  num1: 4,
-  num2: 1,
-  operation: '/',
+const updateInput = function(promptInput) {
+  userInput.num1 = !promptInput.num1 ? userInput.num1 : Number(promptInput.num1);
+  userInput.num2 = !promptInput.num2 ? userInput.num2 : Number(promptInput.num2);
+  userInput.operation = !promptInput.operation ? userInput.operation : promptInput.operation;
 }
 
-if (isNaN(userInput.num1) || isNaN(userInput.num2) || userInput.num2 === 0) {
-  console.log("The numbers you have in mind are invalid");
-  // break;
-}
+const calculateUserInput = function(error, promptInput) {
+  updateInput(promptInput);
 
-if (!operators.includes(userInput.operation)) {
-  console.log("The operation you have in mind are invalid");
-  // break;
-}
+  if (isNaN(userInput.num1) || userInput.num1 === "") {
+    console.log("First number is invalid!");
+    prompt.get(['num1'], calculateUserInput);
+    return;
+  };
 
-const calculate = function(input) {
-  switch(input.operation) {
+  if (isNaN(userInput.num2) || userInput.num2 === "") {
+    console.log("Second number is invalid!");
+    prompt.get(['num2'], calculateUserInput);
+    return;
+  };
+
+  const operators = ["add", "+", "subtract", "-", "multiply", "*", "divide", "/"];
+  if (!operators.includes(userInput.operation)) {
+    console.log("The operation you put in is invalid!");
+    prompt.get(['operation'], calculateUserInput);
+    return;
+  };
+
+  switch(userInput.operation) {
     case "add":
     case "+":
       console.log("We're adding numbers");
-      console.log(`${input.num1} + ${input.num2} : ${input.num1 + input.num2}`);
+      console.log(`${userInput.num1} + ${userInput.num2} : ${userInput.num1 + userInput.num2}`);
       break;
     case "subtract":
     case "-":
       console.log("We're subtracting numbers");
-      console.log(`${input.num1} - ${input.num2} : ${input.num1 - input.num2}`);
+      console.log(`${userInput.num1} - ${userInput.num2} : ${userInput.num1 - userInput.num2}`);
       break;
     case "multiply":
     case "*":
       console.log("We're multiplying numbers");
-      console.log(`${input.num1} * ${input.num2} : ${input.num1 * input.num2}`);
+      console.log(`${userInput.num1} * ${userInput.num2} : ${userInput.num1 * userInput.num2}`);
       break;
     case "divide":
     case "/":
+      if (userInput.num2 === 0) {
+        console.log("The second number can't be zero when dividing!");
+        prompt.get(['num2'], calculateUserInput);
+        return
+      };
       console.log("We're dividing numbers");
-      console.log(`${input.num1} / ${input.num2} : ${input.num1 / input.num2}`);
+      console.log(`${userInput.num1} / ${userInput.num2} : ${userInput.num1 / userInput.num2}`);
       break;
-  }
-}
+  };
 
-calculate(userInput)
+}  
+
+//start the prompt
+prompt.start();
+
+console.log("I am glad you chose my calculator, because it's awesome!");
+console.log("Please give me two numbers and an operator you would like to use from the list below?");
+console.log("1. add(+)");
+console.log("2. subtract(-)");
+console.log("3. multiply(*)");
+console.log("4. divide (/)");
+
+prompt.get(['num1','num2','operation'], calculateUserInput);
 
 
